7cb8c15b4fa34d84078fa474609f53be85c6c3aa
mix23456 <mixsax2@gmail.com>
2023-09-21 13:03:37 +0700

    new: layout

---
+++ lib/view/widgetLayout/homeLayout.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../../constant/assets.dart';
+
+class HomeLayout extends StatelessWidget {
+  final Widget? child;
+  const HomeLayout({
+    super.key,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(Assets.imageBackground), fit: BoxFit.cover)),
+        child: child);
+  }
+}
