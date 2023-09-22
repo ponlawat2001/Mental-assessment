7b3039eb768953da495b5efb099ccd7a98476bf4
mix23456 <mixsax2@gmail.com>
2023-09-22 15:31:37 +0700

    new: Router

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mentalassessment/constant/theme.dart';
+import 'package:mentalassessment/route/routedata.dart';
+import 'package:mentalassessment/view/forgetpassword.dart';
+import 'package:mentalassessment/view/forgetpasswordSuccess.dart';
 import 'package:mentalassessment/view/login.dart';
+import 'package:mentalassessment/view/register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Mental Assessment',
-        theme: theme(),
-        home: const LoginScreen());
+      debugShowCheckedModeBanner: false,
+      title: 'Mental Assessment',
+      theme: theme(),
+      initialRoute: '/',
+      routes: RouteData.routeData,
+    );
   }
 }
---
+++ lib/route/routedata.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:mentalassessment/view/forgetpassword.dart';
+import 'package:mentalassessment/view/forgetpasswordSuccess.dart';
+import 'package:mentalassessment/view/login.dart';
+import 'package:mentalassessment/view/register.dart';
+import 'package:mentalassessment/view/welcome.dart';
+
+class RouteData {
+  static Map<String, Widget Function(BuildContext)> routeData = {
+    '/': (context) => const WelcomeScreen(),
+    '/login': (context) => const LoginScreen(),
+    '/register': (context) => const RegisterScreen(),
+    '/forget': (context) => const ForgetPasswordScreen(),
+    '/forgetsuccess': (context) => const ForgetPasswordSuccessScreen(),
+  };
+}
