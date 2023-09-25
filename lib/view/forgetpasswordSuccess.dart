e77b745e88633755bda14f7f49d8341f894b30a9
mix23456 <mixsax2@gmail.com>
2023-09-25 14:14:22 +0700

    fix: refactor

--- lib/view/forgetpasswordSuccess.dart
+++ lib/view/forgetpasswordSuccess.dart
@@ -18,47 +18,51 @@ class _ForgetPasswordSuccessScreenState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LoginLayout(
-          child: SafeArea(
-              child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 36),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              Assets.iconSuccess,
-              width: 48,
-              height: 48,
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'ส่งอีเมลรีเซ็ทรหัสผ่านสำเร็จ',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'คุณสามารถใช้รหัสผ่านใหม่สำหรับ \nเข้าสู่ระบบบัญชีของคุณ',
-              textAlign: TextAlign.center,
+      body: body(context),
+    );
+  }
+
+  LoginLayout body(BuildContext context) {
+    return LoginLayout(
+        child: SafeArea(
+            child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 36),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            Assets.iconSuccess,
+            width: 48,
+            height: 48,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'ส่งอีเมลรีเซ็ทรหัสผ่านสำเร็จ',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'คุณสามารถใช้รหัสผ่านใหม่สำหรับ \nเข้าสู่ระบบบัญชีของคุณ',
+            textAlign: TextAlign.center,
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall!
+                .copyWith(fontSize: 16, color: ColorTheme.main5),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, '/login'),
+            child: Text(
+              'Sign in',
               style: Theme.of(context)
                   .textTheme
-                  .bodySmall!
-                  .copyWith(fontSize: 16, color: ColorTheme.main5),
+                  .titleLarge!
+                  .copyWith(color: ColorTheme.white),
             ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, '/login'),
-              child: Text(
-                'Sign in',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(color: ColorTheme.white),
-              ),
-            ),
-          ],
-        ),
-      ))),
-    );
+          ),
+        ],
+      ),
+    )));
   }
 }
