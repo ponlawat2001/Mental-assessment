1e1a2dae6e5b8fa3c080de2a4378e2464edce42a
mix23456 <mixsax2@gmail.com>
2023-10-02 13:04:37 +0700

    new: firebasecore init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mentalassessment/constant/theme.dart';
 import 'package:mentalassessment/route/routedata.dart';
+import 'firebase_options.dart';
 
-void main() {
+main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
