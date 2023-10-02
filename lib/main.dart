b37f337021a1bfb34c92cc382cc46c8cb8f7e89f
mix23456 <mixsax2@gmail.com>
2023-10-02 13:30:15 +0700

    fix: gitignore change

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mentalassessment/constant/theme.dart';
 import 'package:mentalassessment/route/routedata.dart';
-import 'firebase_options.dart';
 
-main() async {
+main() {
   runApp(const MyApp());
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 }
 
 class MyApp extends StatelessWidget {
