1ed37c62840eb74cf1ccb24f9c7e2ef5d6220e83
mix23456 <mixsax2@gmail.com>
2023-10-03 15:52:45 +0700

    new: google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mentalassessment/constant/theme.dart';
 import 'package:mentalassessment/route/routedata.dart';
 
-main() {
+main() async {
   runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,5 +1,7 @@
 import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mentalassessment/service/firebase_service.dart';
 import 'package:mentalassessment/view/component/component.dart';
 import '../constant/assets.dart';
 import '../constant/theme.dart';
@@ -13,6 +15,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  UserCredential? googlesignin;
   bool _passwordVisible = true;
   bool _rememberMe = false;
 
@@ -32,130 +35,137 @@ class _LoginScreenState extends State<LoginScreen> {
   Layout body(BuildContext context) {
     return Layout(
       backgroundAsset: Assets.imageBackground,
-      child: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 36),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.asset(
-                Assets.iconLogo,
-                semanticLabel: 'Mental Assessment',
-                width: 100,
-                height: 100,
+      child: SingleChildScrollView(
+        child: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 36),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.asset(
+                  Assets.iconLogo,
+                  semanticLabel: 'Mental Assessment',
+                  width: 100,
+                  height: 100,
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'สวัสดี',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleSmall!
-                  .copyWith(fontSize: 36),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'เจอกันอีกแล้ว วันนี้ก็มาพยายามทำวันนี้ให้เป็นวันที่ดีอีกวันกันเถอะ',
-              textAlign: TextAlign.center,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodySmall!
-                  .copyWith(fontSize: 16, color: ColorTheme.main5),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              validator: (value) {
-                return EmailValidator.validate(value!) ? 'อีเมลไม่ถูกต้อง' : '';
-              },
-              style: Theme.of(context).inputDecorationTheme.labelStyle,
-              cursorColor: ColorTheme.main5,
-              decoration: const InputDecoration(hintText: 'Enter your email..'),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              style: Theme.of(context).inputDecorationTheme.labelStyle,
-              cursorColor: ColorTheme.main5,
-              obscureText: _passwordVisible,
-              decoration: InputDecoration(
-                  hintText: 'Enter your password..',
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _passwordVisible = !_passwordVisible;
-                        });
-                      },
-                      icon: Icon(_passwordVisible
-                          ? Icons.visibility_off_outlined
-                          : Icons.visibility))),
-            ),
-            //  Remember Me
-            rememberMe(context),
-
-            // Sign in Button
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, '/navigator'),
-              child: Text(
-                'Sign in',
+              const SizedBox(height: 8),
+              Text(
+                'สวัสดี',
                 style: Theme.of(context)
                     .textTheme
-                    .titleLarge!
-                    .copyWith(color: ColorTheme.white),
+                    .titleSmall!
+                    .copyWith(fontSize: 36),
               ),
-            ),
-
-            // Divider --Or--
-            Component.dividerOr(context),
-            //Signin with Google Button
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: ColorTheme.main10,
-                backgroundColor: ColorTheme.white,
+              const SizedBox(height: 8),
+              Text(
+                'เจอกันอีกแล้ว วันนี้ก็มาพยายามทำวันนี้ให้เป็นวันที่ดีอีกวันกันเถอะ',
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall!
+                    .copyWith(fontSize: 16, color: ColorTheme.main5),
               ),
-              onPressed: () {},
-              child: Component.socialTextButton(
-                  context, Assets.iconGoogleLogo, 'Continue with Google'),
-            ),
-            const SizedBox(height: 8),
-
-            //Signin with Facebook Button
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: ColorTheme.main10,
-                backgroundColor: ColorTheme.white,
+              const SizedBox(height: 8),
+              TextFormField(
+                validator: (value) {
+                  return EmailValidator.validate(value!)
+                      ? 'อีเมลไม่ถูกต้อง'
+                      : '';
+                },
+                style: Theme.of(context).inputDecorationTheme.labelStyle,
+                cursorColor: ColorTheme.main5,
+                decoration:
+                    const InputDecoration(hintText: 'Enter your email..'),
               ),
-              onPressed: () {},
-              child: Component.socialTextButton(
-                  context, Assets.iconFacebookLogo, 'Continue with Facebook'),
-            ),
-            const SizedBox(height: 16),
+              const SizedBox(height: 8),
+              TextFormField(
+                style: Theme.of(context).inputDecorationTheme.labelStyle,
+                cursorColor: ColorTheme.main5,
+                obscureText: _passwordVisible,
+                decoration: InputDecoration(
+                    hintText: 'Enter your password..',
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                        icon: Icon(_passwordVisible
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility))),
+              ),
+              //  Remember Me
+              rememberMe(context),
 
-            // Register Sign up
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account?",
-                  style: Theme.of(context).textTheme.bodyLarge,
+              // Sign in Button
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, '/navigator'),
+                child: Text(
+                  'Sign in',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(color: ColorTheme.white),
                 ),
-                const SizedBox(width: 4),
-                InkWell(
-                  onTap: () => Navigator.pushNamed(context, '/register'),
-                  child: Text(
-                    'Sign up',
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                          fontWeight: FontWeight.w600,
-                          decoration: TextDecoration.underline,
-                        ),
+              ),
+
+              // Divider --Or--
+              Component.dividerOr(context),
+              //Signin with Google Button
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: ColorTheme.main10,
+                  backgroundColor: ColorTheme.white,
+                ),
+                onPressed: () async {
+                  FirebaseService.signInWithGoogle(context);
+                },
+                child: Component.socialTextButton(
+                    context, Assets.iconGoogleLogo, 'Continue with Google'),
+              ),
+              const SizedBox(height: 8),
+
+              //Signin with Facebook Button
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: ColorTheme.main10,
+                  backgroundColor: ColorTheme.white,
+                ),
+                onPressed: () {},
+                child: Component.socialTextButton(
+                    context, Assets.iconFacebookLogo, 'Continue with Facebook'),
+              ),
+              const SizedBox(height: 16),
+
+              // Register Sign up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: Theme.of(context).textTheme.bodyLarge,
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      )),
+                  const SizedBox(width: 4),
+                  InkWell(
+                    onTap: () => Navigator.pushNamed(context, '/register'),
+                    child: Text(
+                      'Sign up',
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            fontWeight: FontWeight.w600,
+                            decoration: TextDecoration.underline,
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )),
+      ),
     );
   }
 
