ff3d084413adedf3151939bed1a6bf27e1761b94
mix23456 <mixsax2@gmail.com>
2023-10-13 21:30:02 +0700

    fix: firebase core fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -6,7 +8,16 @@ import 'package:mentalassessment/routes/routedata.dart';
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: 'AIzaSyCCkKwU9q0ezz-Y4FfbIudaBNw39_8UpK8',
+              appId:
+                  '48737335927-e5j6it49sjhnpndn82sa5otgibkmc5dv.apps.googleusercontent.com',
+              messagingSenderId: '48737335927',
+              projectId: 'mental-assessment'),
+        )
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
