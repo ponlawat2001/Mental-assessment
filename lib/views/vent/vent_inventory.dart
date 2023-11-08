2c5bead1c81ee18c4f0fbb2a6abe31f77b1d96c7
mix23456 <mixsax2@gmail.com>
2023-11-08 20:06:09 +0700

    new: arrowback & text

--- lib/views/vent/vent_inventory.dart
+++ lib/views/vent/vent_inventory.dart
@@ -12,6 +12,39 @@ class VentInventoryScreen extends StatefulWidget {
 class _VentInventoryScreenState extends State<VentInventoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Layout(backgroundAsset: Assets.imageBackground3);
+    return Scaffold(
+      body: body(),
+    );
   }
+
+  Layout body() => Layout(
+        backgroundAsset: Assets.imageBackground3,
+        child: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Stack(
+            children: [
+              IconButton(
+                visualDensity: VisualDensity.compact,
+                onPressed: () => Navigator.pop(context),
+                icon: const Icon(Icons.arrow_back_rounded),
+              ),
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 52,
+                  ),
+                  Text(
+                    'คลังความรู้สึก',
+                    style: Theme.of(context)
+                        .textTheme
+                        .displaySmall!
+                        .copyWith(fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )),
+      );
 }
