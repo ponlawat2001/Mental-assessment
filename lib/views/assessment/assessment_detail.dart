2123063e0f78d39f574f8a775c4102e367aa14c2
mix23456 <mixsax2@gmail.com>
2024-01-08 11:16:51 +0700

    new: handle new choices

--- lib/views/assessment/assessment_detail.dart
+++ lib/views/assessment/assessment_detail.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:mentalassessment/constants/assets.dart';
 import 'package:mentalassessment/controllers/assessment_controller.dart';
 import 'package:mentalassessment/controllers/task_controller.dart';
+import 'package:mentalassessment/model/assessment/assessment_model.dart';
 import 'package:mentalassessment/model/assessment/history_model.dart';
 import 'package:mentalassessment/services/history_service.dart';
 import 'package:mentalassessment/services/task_service.dart';
@@ -132,147 +133,263 @@ class _AssessmentDetailScreenState extends State<AssessmentDetailScreen> {
                     ],
                   ),
                   SizedBox(height: gap),
-                  Wrap(
-                    runSpacing: 16,
-                    spacing: 16,
-                    children: controller.assessment[args].answer!
-                        .asMap()
-                        .entries
-                        .map((e) {
-                      return InkWell(
-                        onTap: () async {
-                          SharedPreferences prefs =
-                              await SharedPreferences.getInstance();
-                          //is mainAssessment
-                          if (prefs.getString('createTaskId') != null) {
-                            if (counter !=
-                                controller
-                                    .assessment[args].questionnaire!.length) {
-                              taskController.pushAnswer(
-                                  controller.assessment[args]
-                                      .questionnaire![counter - 1],
-                                  e,
-                                  controller.assessment[args].answer,
-                                  controller.assessment[args]);
-                            } else {
-                              //outofQ
-                              if (assessmentController.assessment.length ==
-                                  args + 1) {
-                                taskController.pushAnswer(
-                                    controller.assessment[args]
-                                        .questionnaire![counter - 1],
-                                    e,
-                                    controller.assessment[args].answer,
-                                    controller.assessment[args]);
-                                if (!context.mounted) return;
-                                // update task to firestore
-                                await TaskService.updateTask(
-                                    taskController.summary.value,
-                                    prefs.getString('createTaskId')!,
-                                    context);
-                                if (!context.mounted) return;
-                                AlertDialogselect.loadingDialog(context);
-                                HistoryResult? res = await TaskService.findOne(
-                                    prefs.getString('createTaskId') ?? '');
-                                HistoryService.createHistory(res).then((value) {
-                                  TaskService.deleteTask(
-                                      prefs.getString('createTaskId') ?? '',
-                                      context);
-                                  Navigator.pushReplacementNamed(
-                                      context, '/assessmenthistorydetail',
-                                      arguments: value);
-                                });
-                              } else {
-                                taskController.pushAnswer(
-                                    controller.assessment[args]
-                                        .questionnaire![counter - 1],
-                                    e,
-                                    controller.assessment[args].answer,
-                                    controller.assessment[args]);
-                                if (!context.mounted) return;
-                                TaskService.updateTask(
-                                    taskController.summary.value,
-                                    prefs.getString('createTaskId') ?? '',
-                                    context);
-                                Navigator.pop(context);
-                                Navigator.pushReplacementNamed(
-                                    context, '/assessmentdescription',
-                                    arguments: args + 1);
-                              }
-                            }
-                            if (counter !=
-                                controller
-                                    .assessment[args].questionnaire!.length) {
-                              setState(() {
-                                counter++;
-                              });
-                            }
-                          } else {
-                            //other assessement method
-                            if (counter !=
-                                controller
-                                    .assessment[args].questionnaire!.length) {
-                              taskController.pushAnswer(
-                                  controller.assessment[args]
-                                      .questionnaire![counter - 1],
-                                  e,
-                                  controller.assessment[args].answer,
-                                  controller.assessment[args]);
-                            } else {
-                              taskController.pushAnswer(
-                                  controller.assessment[args]
-                                      .questionnaire![counter - 1],
-                                  e,
-                                  controller.assessment[args].answer,
-                                  controller.assessment[args]);
-                              if (!context.mounted) return;
-                              AlertDialogselect.loadingDialog(context);
-                              await HistoryService.createHistory(HistoryResult(
-                                      owner: FirebaseAuth
-                                          .instance.currentUser!.email,
-                                      summary: [taskController.summary.value],
-                                      type: 'other'))
-                                  .then((value) {
-                                Navigator.pushReplacementNamed(
-                                    context, '/assessmenthistorydetail',
-                                    arguments: value);
-                              });
-                            }
-                            if (counter !=
-                                controller
-                                    .assessment[args].questionnaire!.length) {
-                              setState(() {
-                                counter++;
-                              });
-                            }
-                          }
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: ColorTheme.main30,
-                              borderRadius: BorderRadius.circular(16),
-                              boxShadow: [
-                                BoxShadow(
-                                    offset: const Offset(0, 4),
-                                    blurRadius: 2,
-                                    spreadRadius: 0,
-                                    color: ColorTheme.lightGray2),
-                              ]),
-                          padding: const EdgeInsets.all(16),
-                          child: Text(
-                            e.value.name ?? 'Unknown',
-                            style: Theme.of(context).textTheme.titleSmall,
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  )
+                  controller.assessment[args].answer?.length ==
+                          controller.assessment[args].questionnaire?.length
+                      ? choiceWhenqusetionown(
+                          controller,
+                          args,
+                          counter - 1,
+                          controller.assessment[args]
+                                  .questionnaire?[counter - 1].id ??
+                              0,
+                          context)
+                      : choiceWhenyesorno(controller, args, context)
                 ],
               );
             },
           ),
         ),
       ),
+    );
+  }
+
+  Wrap choiceWhenqusetionown(AssessmentController controller, int args,
+      int index, int questionid, BuildContext context) {
+    Answer temp = controller.assessment[args].answer!
+        .firstWhere((element) => element.id == questionid);
+    return Wrap(
+      runSpacing: 16,
+      spacing: 16,
+      children: temp.choices!.asMap().entries.map((e) {
+        return InkWell(
+          onTap: () async {
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            //is mainAssessment
+            if (prefs.getString('createTaskId') != null) {
+              if (counter !=
+                  controller.assessment[args].questionnaire!.length) {
+                taskController.pushAnswer(
+                    controller.assessment[args].questionnaire![counter - 1],
+                    e,
+                    controller.assessment[args].answer,
+                    controller.assessment[args]);
+              } else {
+                //outofQ
+                if (assessmentController.assessment.length == args + 1) {
+                  taskController.pushAnswer(
+                      controller.assessment[args].questionnaire![counter - 1],
+                      e,
+                      controller.assessment[args].answer,
+                      controller.assessment[args]);
+                  if (!context.mounted) return;
+                  // update task to firestore
+                  await TaskService.updateTask(taskController.summary.value,
+                      prefs.getString('createTaskId')!, context);
+                  if (!context.mounted) return;
+                  AlertDialogselect.loadingDialog(context);
+                  HistoryResult? res = await TaskService.findOne(
+                      prefs.getString('createTaskId') ?? '');
+                  HistoryService.createHistory(res).then((value) {
+                    TaskService.deleteTask(
+                        prefs.getString('createTaskId') ?? '', context);
+                    Navigator.pushReplacementNamed(
+                        context, '/assessmenthistorydetail',
+                        arguments: value);
+                  });
+                } else {
+                  taskController.pushAnswer(
+                      controller.assessment[args].questionnaire![counter - 1],
+                      e,
+                      controller.assessment[args].answer,
+                      controller.assessment[args]);
+                  if (!context.mounted) return;
+                  TaskService.updateTask(taskController.summary.value,
+                      prefs.getString('createTaskId') ?? '', context);
+                  Navigator.pop(context);
+                  Navigator.pushReplacementNamed(
+                      context, '/assessmentdescription',
+                      arguments: args + 1);
+                }
+              }
+              if (counter !=
+                  controller.assessment[args].questionnaire!.length) {
+                setState(() {
+                  counter++;
+                });
+              }
+            } else {
+              //other assessement method
+              if (counter !=
+                  controller.assessment[args].questionnaire!.length) {
+                taskController.pushAnswer(
+                    controller.assessment[args].questionnaire![counter - 1],
+                    e,
+                    controller.assessment[args].answer,
+                    controller.assessment[args]);
+              } else {
+                taskController.pushAnswer(
+                    controller.assessment[args].questionnaire![counter - 1],
+                    e,
+                    controller.assessment[args].answer,
+                    controller.assessment[args]);
+                if (!context.mounted) return;
+                AlertDialogselect.loadingDialog(context);
+                await HistoryService.createHistory(HistoryResult(
+                        owner: FirebaseAuth.instance.currentUser!.email,
+                        summary: [taskController.summary.value],
+                        type: 'other'))
+                    .then((value) {
+                  Navigator.pushReplacementNamed(
+                      context, '/assessmenthistorydetail',
+                      arguments: value);
+                });
+              }
+              if (counter !=
+                  controller.assessment[args].questionnaire!.length) {
+                setState(() {
+                  counter++;
+                });
+              }
+            }
+          },
+          child: Container(
+            decoration: BoxDecoration(
+                color: ColorTheme.main30,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                      offset: const Offset(0, 4),
+                      blurRadius: 2,
+                      spreadRadius: 0,
+                      color: ColorTheme.lightGray2),
+                ]),
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              e.value.name ?? 'Unknown',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  Wrap choiceWhenyesorno(
+      AssessmentController controller, int args, BuildContext context) {
+    return Wrap(
+      runSpacing: 16,
+      spacing: 16,
+      children: controller.assessment[args].answer!.asMap().entries.map((e) {
+        return InkWell(
+          onTap: () async {
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            //is mainAssessment
+            if (prefs.getString('createTaskId') != null) {
+              if (counter !=
+                  controller.assessment[args].questionnaire!.length) {
+                taskController.pushAnswer(
+                    controller.assessment[args].questionnaire![counter - 1],
+                    e,
+                    controller.assessment[args].answer,
+                    controller.assessment[args]);
+              } else {
+                //outofQ
+                if (assessmentController.assessment.length == args + 1) {
+                  taskController.pushAnswer(
+                      controller.assessment[args].questionnaire![counter - 1],
+                      e,
+                      controller.assessment[args].answer,
+                      controller.assessment[args]);
+                  if (!context.mounted) return;
+                  // update task to firestore
+                  await TaskService.updateTask(taskController.summary.value,
+                      prefs.getString('createTaskId')!, context);
+                  if (!context.mounted) return;
+                  AlertDialogselect.loadingDialog(context);
+                  HistoryResult? res = await TaskService.findOne(
+                      prefs.getString('createTaskId') ?? '');
+                  HistoryService.createHistory(res).then((value) {
+                    TaskService.deleteTask(
+                        prefs.getString('createTaskId') ?? '', context);
+                    Navigator.pushReplacementNamed(
+                        context, '/assessmenthistorydetail',
+                        arguments: value);
+                  });
+                } else {
+                  taskController.pushAnswer(
+                      controller.assessment[args].questionnaire![counter - 1],
+                      e,
+                      controller.assessment[args].answer,
+                      controller.assessment[args]);
+                  if (!context.mounted) return;
+                  TaskService.updateTask(taskController.summary.value,
+                      prefs.getString('createTaskId') ?? '', context);
+                  Navigator.pop(context);
+                  Navigator.pushReplacementNamed(
+                      context, '/assessmentdescription',
+                      arguments: args + 1);
+                }
+              }
+              if (counter !=
+                  controller.assessment[args].questionnaire!.length) {
+                setState(() {
+                  counter++;
+                });
+              }
+            } else {
+              //other assessement method
+              if (counter !=
+                  controller.assessment[args].questionnaire!.length) {
+                taskController.pushAnswer(
+                    controller.assessment[args].questionnaire![counter - 1],
+                    e,
+                    controller.assessment[args].answer,
+                    controller.assessment[args]);
+              } else {
+                taskController.pushAnswer(
+                    controller.assessment[args].questionnaire![counter - 1],
+                    e,
+                    controller.assessment[args].answer,
+                    controller.assessment[args]);
+                if (!context.mounted) return;
+                AlertDialogselect.loadingDialog(context);
+                await HistoryService.createHistory(HistoryResult(
+                        owner: FirebaseAuth.instance.currentUser!.email,
+                        summary: [taskController.summary.value],
+                        type: 'other'))
+                    .then((value) {
+                  Navigator.pushReplacementNamed(
+                      context, '/assessmenthistorydetail',
+                      arguments: value);
+                });
+              }
+              if (counter !=
+                  controller.assessment[args].questionnaire!.length) {
+                setState(() {
+                  counter++;
+                });
+              }
+            }
+          },
+          child: Container(
+            decoration: BoxDecoration(
+                color: ColorTheme.main30,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                      offset: const Offset(0, 4),
+                      blurRadius: 2,
+                      spreadRadius: 0,
+                      color: ColorTheme.lightGray2),
+                ]),
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              e.value.name ?? 'Unknown',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+          ),
+        );
+      }).toList(),
     );
   }
 }
